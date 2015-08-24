81d9108c9756cb09f554e15f236fcdaea6d2c77a
travis crampton & clayton easterbrooks <student@epicodus.com>
2015-08-24 16:43:29 -0700

    add players and delete

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ get("/league") do
 end
 
 get('/teams') do
-
+  @teams = Team.all
   erb(:teams)
 end
 
@@ -40,4 +40,25 @@ delete('/league') do
   team.destroy()
   @teams = Team.all()
   erb(:league)
+end
+
+get('/teams/:id') do
+  @team = Team.find(params.fetch('id').to_i())
+  erb(:manage_team)
+end
+
+post('/teams/:id') do
+  player_name = params.fetch("player_name")
+  @team = Team.find(params.fetch("id").to_i())
+  @team.players().create({:name => player_name})
+  @players = Team.all()
+  erb(:manage_team)
+end
+
+delete('/teams/:id') do
+  player = Player.find(params.fetch('players_delete').to_i())
+  player.destroy()
+  @players = Player.all()
+  @team = Team.find(params.fetch('id').to_i())
+  erb(:manage_team)
 end
